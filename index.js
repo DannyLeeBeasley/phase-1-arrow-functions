62c86de85629592b0cb8309a1850de8a1383f074
Dan Beasley <dannyleebeasley@gmail.com>
2021-06-13 15:50:02 -0500

    second submission

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const divide = function() {
-    return 2000 / 100
+function divide(){
+    return 2000/100;
 }
 
-const square = (x) => x * x
+const square = num => num * num
 
-const add = (x, y) => x + y
+const add = (num1, num2) => num1 + num2
